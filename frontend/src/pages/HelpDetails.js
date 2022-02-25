d38c21bc014d479e0de61c659fbb13ea1f7621c6
Mike L <firyace@gmail.com>
2022-02-25 13:23:15 -0700

    build: updated helplist to go to actual survey

--- frontend/src/pages/HelpDetails.js
+++ frontend/src/pages/HelpDetails.js
@@ -2,20 +2,36 @@ import React from 'react';
 import { useFetch } from '../helpers/useFetch';
 import { Link } from 'react-router-dom';
 import { getSurvey } from '../services/fakeSurveyServices';
-const HelpList = () => {
+import Sidebar from './../components/Sidebar';
+import Header from './../components/Header';
+import Footer from './../components/Footer';
+import ScrollTop from './../components/ScrollTop';
+import HelpCard from '../components/HealthCard';
+const HelpDetails = () => {
+
 
     return (
-      <div className='row'>
-        <div className='col-lg-10'>
-          <div className='card shadow mb-4'>
-            <div className='card-body'>
+        <React.Fragment>
+        <div id='wrapper'>
+          <Sidebar />
+
+          <div id='content-wrapper' className='d-flex flex-column'>
+            <div id='content'>
+              <Header heading={getSurvey()[0]['name']} />
+              <div className='container-fluid'>
+                  <HelpCard />
 
 
+              </div>
             </div>
+
+            <Footer />
           </div>
         </div>
-      </div>
+
+        <ScrollTop />
+      </React.Fragment>
     );
   };
 
-  export default HelpList;
+  export default HelpDetails;
