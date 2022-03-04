dabc3ef196c9ae9c5618c1d73940c1db720138b4
Bhavyai Gupta <zbhavyai@gmail.com>
2022-03-03 21:46:15 -0700

    securing endpoints with springsecurity and jwt

---
+++ backend/src/main/java/ml/intellirn/api/service/AdminService.java
@@ -0,0 +1,30 @@
+package ml.intellirn.api.service;
+
+import ml.intellirn.api.model.*;
+import ml.intellirn.api.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AdminService implements UserDetailsService {
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        List<Admin> allAdmins = this.adminRepository.findAll();
+
+        for (Admin eachAdmin : allAdmins) {
+            if (eachAdmin.getEmail().equalsIgnoreCase(username)) {
+                return new User(username, eachAdmin.getPassword(), new ArrayList<>());
+            }
+        }
+
+        return null;
+    }
+}
